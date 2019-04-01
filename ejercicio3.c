f491d42668dabaec9d18e7c0232f7cf28d037560
san7iix <xsantysxx@gmail.com>
2019-04-01 02:38:19 -0500

    Actualización de ejercicio

--- ejercicio3.c
+++ ejercicio3.c
@@ -3,14 +3,15 @@
 #include <string.h>
 #include <sys/types.h>
 #include <wait.h>
+#include <stdlib.h>
 
 #define MAX_BUFF 1024
-int i;
+int i,contadorVocales=0,indice;
+char buff[MAX_BUFF],letra[1];
 
 int main(int argc, char const *argv[]) {
 
   int tuberia1[2],tuberia2[2],n;
-  char buff[MAX_BUFF];
   pipe(tuberia1);
   pipe(tuberia2);
 
@@ -19,13 +20,13 @@ int main(int argc, char const *argv[]) {
       break;
     }
   }
-
   //Lectura 0 escritura 1
   if(i==2){
     close(tuberia1[1]);
     close(tuberia2[1]);
     close(tuberia2[0]);
     wait(NULL);
+    getchar();
 
   }else if(i==1){
     close(tuberia1[1]);
@@ -38,11 +39,24 @@ int main(int argc, char const *argv[]) {
       write(tuberia2[1],buff,sizeof(buff));
       fgets(buff,MAX_BUFF,archivo);
     }
-    printf("[%d]Archivo enviado por tuberia\n",getpid() );
+    printf("[%d]Archivo enviado por tuberia\n",getpid());
     fclose(archivo);
     close(tuberia2[1]);
   }else{
+    close(tuberia1[0]);
+    close(tuberia2[1]);
 
+    while((n=read(tuberia2[0],buff,MAX_BUFF))>0){
+      contadorVocales=0;
+      buff[n] = '\0';
+      printf("[Hijo 1][%d] Lee: %s",getpid(),buff);
+      if(strchr(buff,'a'))contadorVocales++;
+      printf("%i\n",contadorVocales );
+    }
+
+    close(tuberia1[1]);
+    close(tuberia2[0]);
   }
 
+  return 0;
 }
