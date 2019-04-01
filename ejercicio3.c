c9497450005bcd55be8105ac2f0dd5b4c943ab75
san7iix <xsantysxx@gmail.com>
2019-04-01 01:47:56 -0500

    Actualizacion ejercicio

--- ejercicio3.c
+++ ejercicio3.c
@@ -5,9 +5,6 @@
 #include <wait.h>
 
 #define MAX_BUFF 1024
-
-char *ping = "PING";
-char *pong = "PONG";
 int i;
 
 int main(int argc, char const *argv[]) {
@@ -27,51 +24,25 @@ int main(int argc, char const *argv[]) {
   if(i==2){
     close(tuberia1[1]);
     close(tuberia2[1]);
+    close(tuberia2[0]);
     wait(NULL);
-    printf("-----------Archivo1---------------\n");
-    while((n=read(tuberia2[0],buff,MAX_BUFF))>0){
-      buff[n] = '\0';
-      printf("[Padre][%d] Lee: %s",getpid(),buff);
-    }
 
-    printf("-----------Archivo2---------------\n");
-
-    while((n=read(tuberia1[0],buff,MAX_BUFF))>0){
-      buff[n] = '\0';
-      printf("[Padre][%d] Lee: %s",getpid(),buff);
-    }
-    close(tuberia2[0]);
-    close(tuberia1[0]);
   }else if(i==1){
-    close(tuberia2[0]);
     close(tuberia1[1]);
     close(tuberia1[0]);
-    printf("Proceso 2 envia por tuberia 2\n");
+    close(tuberia2[0]);
     FILE *archivo;
     archivo = fopen("archivo.txt","r");
-
     fgets(buff,MAX_BUFF,archivo);
-    while (!feof(archivo)){
-     write(tuberia2[1],buff,sizeof(buff));
-     fgets(buff,MAX_BUFF,archivo);
+    while (!feof(archivo)) {
+      write(tuberia2[1],buff,sizeof(buff));
+      fgets(buff,MAX_BUFF,archivo);
     }
-    close(tuberia2[0]);
-
-  }else{
-    close(tuberia1[0]);
+    printf("[%d]Archivo enviado por tuberia\n",getpid() );
+    fclose(archivo);
     close(tuberia2[1]);
-    close(tuberia2[0]);
+  }else{
 
-    printf("Proceso 1 envia por tuberia 1\n");
-    FILE *archivo;
-    archivo = fopen("archivo2.txt","r");
-
-    fgets(buff,MAX_BUFF,archivo);
-    while (!feof(archivo)){
-     write(tuberia1[1],buff,sizeof(buff));
-     fgets(buff,MAX_BUFF,archivo);
-    }
-    close(tuberia1[0]);
   }
 
 }
